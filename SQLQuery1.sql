3a2778ad09832a3e46a913a6bad7e96444a9cd88
SaadWhySaad <156309384+SaadWhySaad@users.noreply.github.com>
2024-06-03 01:18:24 +0500

    Add files via upload
    Another Query

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,35 +1,56 @@
-﻿create table restaurant(
-	r_id int primary key identity,
-	r_name nvarchar(100) not null,
-	r_description nvarchar(150),
-	r_admin nvarchar(450),
-	FOREIGN KEY (r_admin) REFERENCES AspNetUsers(Id)
-);
+use diningSystem
 
 
-insert into restaurant(r_name, r_description, r_admin)
-values('CAFE HAWAII', 'Cafe Hawaii Restaurant blends the flavors of the islands with international cuisine in Karachi.', 'd6ce7ed4-4dae-4aab-84e3-8c9c31f2e3ce');
+select * from Orders
 
-select * from restaurant
+drop table Orders
 
-
-create table restaurantMenu(
-	menu_id int identity,
-	menu_item nvarchar(50),
-	menu_description nvarchar(200),
-	menu_item_price int not null,
+create table Orders (
+	orderId int primary key identity,
+	UserId nvarchar(450),
+	OrderType varchar(50),
+	DineInDate Date,
+	DineInTime Time,
+	NumberOfPersons int,
+	DeliveryAddress nvarchar(450),
+	CardNumber varchar(20),
+	ExpirationDate varchar(5),
+	CVV varchar(3),
+	order_status varchar(10),
 	r_id int,
+	foreign key (UserId) references AspNetUsers(Id),
 	foreign key (r_id) references restaurant(r_id)
 );
 
-drop table restaurantMenu
 
-select * from restaurantMenu
+SELECT o.orderId, o.UserId, u.FirstName + ' ' + u.LastName AS UserName, o.OrderType, o.DineInDate, o.DineInTime, o.NumberOfPersons, o.DeliveryAddress, o.CardNumber, o.ExpirationDate, o.CVV, o.order_status, o.r_id
+                    FROM Orders o
+                    JOIN Restaurant r ON o.r_id = r.r_id
+                    JOIN AspNetUsers u ON o.UserId = u.Id
+                    WHERE r.r_admin = u.Id
 
-insert into restaurantMenu values('Cappuccino Coffee', 'Cappuccino: Rich espresso topped with frothy milk, a delightful blend of bold and creamy flavors.', 350, 3);
 
-select * from restaurantMenu where r_id = 3;
 
+select * from CartItems
+
+select * from Orders
+insert into Orders values ('2cb8c16c-f5df-4a33-8012-8bee1c819471', 'DineIn', '2024-05-31', '03:00:00.0000000', 4, 'CE17 Airport, Malir Halt', '4242 4242 4242 4242', '01/25', 123, 'pending', 4)
 select * from AspNetUsers
 
-delete from AspNetUsers where FirstName = 'm'
+
+SELECT o.orderId, o.UserId, u.UserName, o.OrderType, o.DineInDate, o.DineInTime, o.NumberOfPersons, o.DeliveryAddress, o.CardNumber, o.ExpirationDate, o.CVV, o.order_status, r.r_name
+                    FROM Orders o
+                    JOIN Restaurant r ON o.r_id = r.r_id
+                    JOIN AspNetUsers u ON o.UserId = u.Id
+                    WHERE r.r_admin = u.Id;
+
+select o.orderId, o.UserId, o.OrderType, o.DineInDate, o.DineInTime, o.NumberOfPersons, o.DeliveryAddress, o.CardNumber, o.ExpirationDate, o.CVV, o.order_status, r.r_name
+				from orders o
+				join restaurant r on o.r_id = r.r_id
+				join AspNetUsers u on o.UserId = u.Id
+				where r.r_admin = u.Id;
+
+
+
+
+
